653b2a0d76ab64db7c562d6d94a87c15c6e3803e
meavinash05 <meavinash05@gmail.com>
2020-06-04 23:42:59 +1000

    Updated the Heroku link

--- src/components/FrontController/FrontController.js
+++ src/components/FrontController/FrontController.js
@@ -28,7 +28,7 @@ class FrontController extends Component {
     signUpHandler = (userData) => {
         alert('Front Controller Sign Up Handler')
         console.log(userData)
-        fetch('http://localhost:8080/addUser', {
+        fetch('https://apartmentportal.herokuapp.com/addUser', {
             method: 'POST',
             body: JSON.stringify(userData),
             headers: { 'Content-Type': 'application/json' }
